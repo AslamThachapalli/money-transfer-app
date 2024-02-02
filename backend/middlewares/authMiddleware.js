b7e2f1aadec527abbf315d0d797d322c12699993
AslamThachapalli <aslam.develop912@gmail.com>
2024-02-02 15:11:12 +0300

    api integrations

--- backend/middlewares/authMiddleware.js
+++ backend/middlewares/authMiddleware.js
@@ -2,13 +2,16 @@ const jwt = require('jsonwebtoken');
 const { JWT_SECRET } = require('../config');
 
 function authMiddleware(req, res, next) {
+    console.log('authenticating')
     let authHeader = req.headers.authorization;
 
     if(!authHeader || !authHeader.startsWith('Bearer ')){
         return res.status(403).json({});
     }
 
+    console.log(authHeader)
     const token = authHeader.split(" ")[1];
+    console.log(token)
 
     try{
         const decoded = jwt.verify(token, JWT_SECRET);
